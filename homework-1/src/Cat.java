ef6315f348ad12bd1ecd1d08699c6efde7779151
Diego Cruz <diego.rubencruz@gmail.com>
2021-02-06 17:09:43 -0800

    Created a gitignore file that would ignore a redundant txt file within a folder

--- homework-1/src/Cat.java
+++ homework-1/src/Cat.java
@@ -7,8 +7,22 @@
 public class Cat
 {
     private int age;
-
+    
+    /**
+     * Basic constructor that takes input of user with respect to age.
+     *
+     * @param  ageToInit  The age that will be assigned to the cat object.
+     */
     public Cat(int ageToInit){
         this.age = ageToInit;
+    }
+
+    /**
+     * Utility operation to fetch the age of the cat.
+     *
+     * @return  the age of the cat.
+     */
+    public int getAge(){
+        return this.age;
     }
 }
