b3ae833f23ab520704bb3105088b473c9c66cd6a
Diego Cruz <diego.rubencruz@gmail.com>
2021-04-20 17:07:51 -0700

    Previous commit did not add files to repo, this fixes that

---
+++ src/FreeHours.java
@@ -0,0 +1,20 @@
+package src;
+
+public class FreeHours {
+    private int[][] timeline;
+
+    public void updateTime(int day, int hour){
+        //this is gonna get figured out
+    }
+
+    public void clearDay(int day){
+        //this will get handled
+    }
+    
+    public void getAvailableHours(){
+        //this needs more thought, whether we want to output something
+        //of an elaborate string into the terminal for early dev, or
+        //whether we want to store it somewhere where it will get displayed
+        //on the html side of things
+    }
+}
