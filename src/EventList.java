45f18180fa54469d036b10e883100bd0bc4fa439
Dylan Mirandilla <apmirandilla@gmail.com>
2021-04-24 16:00:54 -0700

    added javadoc for FreeHours class

--- src/EventList.java
+++ src/EventList.java
@@ -5,18 +5,19 @@ import java.util.ArrayList;
 public class EventList {
     private ArrayList<Event> eventsList;
 
-    public void sort(){
-        //Likely will sort by date/time, then by name, then by other sorting criteria
-        //Likely going to implement the comparisons made between objects of event class
+    public void sort() {
+        // Likely will sort by date/time, then by name, then by other sorting criteria
+        // Likely going to implement the comparisons made between objects of event class
     }
 
-    public void deleteEvent(String eventName){
-        //this will get done soon, just a matter of whether we want to be able to delete the first match,
-        //or if we want to delete multiple events that share the same name
+    public void deleteEvent(String eventName) {
+        // this will get done soon, just a matter of whether we want to be able to
+        // delete the first match,
+        // or if we want to delete multiple events that share the same name
     }
 
-    public void addEvent(Event eventToAdd){
+    public void addEvent(Event eventToAdd) {
         eventsList.add(eventToAdd);
     }
-    
+
 }
