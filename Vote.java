7b1606c61c5bd128e33737c0fce63d2436862da6
Diego Cruz <diego.rubencruz@gmail.com>
2021-04-24 14:40:54 -0700

    Created User and Vote basic java classes

---
+++ Vote.java
@@ -0,0 +1,17 @@
+public class Vote {
+    private String authorID;
+    private boolean isUpvote;
+
+    public Vote(String inputID, boolean inputType){
+        authorID = inputID;
+        isUpvote = inputType;
+    }
+
+    public String getAuthorID(){
+        return authorID;
+    }
+
+    public void switchValue(){
+        isUpvote = !isUpvote; 
+    }
+}
