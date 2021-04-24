588cbfc815dae7cd63e2cfde41b7080e0913d09e
Diego Cruz <diego.rubencruz@gmail.com>
2021-04-24 15:43:21 -0700

    Added Javadoc to Event and EventList classes

--- src/EventList.java
+++ src/EventList.java
@@ -5,18 +5,33 @@ import java.util.ArrayList;
 public class EventList {
     private ArrayList<Event> eventsList;
 
-    public void sort(){
-        //Likely will sort by date/time, then by name, then by other sorting criteria
-        //Likely going to implement the comparisons made between objects of event class
+    /**
+     * Sorts eventsList by date/time, then by name, then by other sorting criteria.
+     * This will likely implement some comparator for objects.
+     */
+    public void sort() {
+        // need to implement some comparator in either Event class or this method, check
+        // the lecture material.
     }
 
-    public void deleteEvent(String eventName){
-        //this will get done soon, just a matter of whether we want to be able to delete the first match,
-        //or if we want to delete multiple events that share the same name
+    /**
+     * Deletes an event with a specific name from eventsList.
+     * 
+     * @param eventName The name of the event that is to be deleted.
+     */
+    public void deleteEvent(String eventName) {
+        // this will get done soon, just a matter of whether we want to be able to
+        // delete the first match,
+        // or if we want to delete multiple events that share the same name
     }
 
-    public void addEvent(Event eventToAdd){
+    /**
+     * Adds an event object to eventsList.
+     * 
+     * @param eventToAdd The event to be added to eventsList.
+     */
+    public void addEvent(Event eventToAdd) {
         eventsList.add(eventToAdd);
     }
-    
+
 }
