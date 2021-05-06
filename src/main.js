21c26b87a614a7476d9bcc4c85f8aec2c73acc94
Dylan Mirandilla <apmirandilla@gmail.com>
2021-05-06 00:42:10 -0700

    Added homepage, login/logout functionality

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-<html>
-  {" "}
-  hello world !!!!!
-  <p> testing merge </p>
-</html>;
+function onSignIn() {
+  $("#HomePage").css("display", "none");
+  $(".data").css("display", "block");
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    alert("You have been successfully signed out");
+    $(".data").css("display", "none")
+    $("#HomePage").css("display", "flex");
+  });
+}
