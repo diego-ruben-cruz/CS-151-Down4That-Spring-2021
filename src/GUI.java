48c59c89c58106776993cde0f4388ef1de72c253
akeladh <74440025+akeladh@users.noreply.github.com>
2021-05-15 20:27:49 -0700

    Made the GUI with 3 separate columns

--- src/GUI.java
+++ src/GUI.java
@@ -2,72 +2,117 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GUI extends JFrame {
-
     public static void main(String[] args) {
         JFrame frame = new JFrame();
+        frame.setSize(700, 700);
+        //right side of app
+        //buttons
         JButton addEvent = new JButton("Add Event");
         JButton deleteEvent = new JButton("Delete Event");
+        //event input
         JLabel eventNameLabel = new JLabel("Event Name Here");
-        JTextField eventName = new JTextField(10);
-
+        JTextField eventName = new JTextField(50);
+        eventName.setMaximumSize(eventName.getPreferredSize());
+        //location input
         JLabel locationLabel = new JLabel("Event Location Here");
-        JTextField location = new JTextField(10);
-
+        JTextField location = new JTextField(50);
+        location.setMaximumSize(location.getPreferredSize());
+        //time input
         JLabel timeLabel = new JLabel("Event Time Here");
-        JTextField time = new JTextField(10);
-        JLabel timeline = new JLabel("Timeline");
+        JTextField time = new JTextField(50);
+        time.setMaximumSize(time.getPreferredSize());
 
-        JLabel freeHours = new JLabel("Free Hours");
+        //center of app
+        JLabel timelineLabel = new JLabel("Timeline");
 
-        JPanel mainPanel = new JPanel();
-        JPanel rightPanel = new JPanel(); // panel for "EAST"
-        JPanel leftPanel = new JPanel();// panel for "WEST"
+        //left side of app
+        JLabel freeHoursLabel = new JLabel("Free Hours");
+        JTextField freeHours = new JTextField(10);
+        freeHours.setMaximumSize(freeHours.getPreferredSize());
+        JCheckBox mon = new JCheckBox("Monday");
+        JCheckBox tues = new JCheckBox("Tuesday");
+        JCheckBox wed = new JCheckBox("Wednesday");
+        JCheckBox thur = new JCheckBox("Thursday");
+        JCheckBox fri = new JCheckBox("Friday");
+        JCheckBox sat = new JCheckBox("Saturday");
+        JCheckBox sun = new JCheckBox("Sunday");
+        JButton addHour = new JButton("Add Hours");
+        JLabel yourFreeHours = new JLabel("Your Free Hours:");
 
-        //Colors
-        Color custom1 = new Color(158, 219, 235);
-        rightPanel.setBackground(Color.orange);
-        //rightPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-        mainPanel.setBackground(Color.lightGray);
-        //mainPanel.setBorder(BorderFactory.createLineBorder(Color.blue));
-        leftPanel.setBackground(custom1);
-        //leftPanel.setBorder(BorderFactory.createLineBorder(Color.red));
-        //mainPanel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        frame.setLayout(new BorderLayout());
-        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.PAGE_AXIS));
-        leftPanel.setLayout(new FlowLayout());
-        mainPanel.setLayout(new FlowLayout());
+        JPanel center = new JPanel();
+        JPanel left = new JPanel();
+        JPanel right = new JPanel();
+        //set frame into grid layout with grid layout into 3 columns
+        frame.setLayout(new GridLayout(0, 3, 0, 0));
+        frame.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+        frame.add(left);
+        frame.add(center);
+        frame.add(right);
 
-        frame.add(mainPanel, BorderLayout.CENTER);
-        frame.add(rightPanel, BorderLayout.EAST);
-        frame.add(leftPanel, BorderLayout.WEST);
-        leftPanel.add(freeHours);
+        //Jpanel Colors
+        Color cust1 = new Color(255, 153, 153);
+        Color cust2 = new Color(255, 204, 153);
+        Color cust3 = new Color(255, 255, 153);
+        left.setBackground(cust1);
+        center.setBackground(cust2);
+        right.setBackground(cust3);
 
-        mainPanel.add(timeline);
+        //left layout
+        left.setLayout(new BoxLayout(left, BoxLayout.PAGE_AXIS));
+        JPanel left1 = new JPanel();
+        JPanel left2 = new JPanel(); // might not need this panel  to print outputs out
+        left.add(left1);
+        left.add(yourFreeHours);
+        left.add(left2);
 
-        rightPanel.add(Box.createRigidArea(new Dimension(0, 10)));
-        rightPanel.add(eventNameLabel);
-        rightPanel.add(eventName);
-        rightPanel.add(Box.createRigidArea(new Dimension(0, 10)));
-        rightPanel.add(locationLabel);
-        rightPanel.add(location);
-        rightPanel.add(Box.createRigidArea(new Dimension(0, 10)));
-        rightPanel.add(timeLabel);
-        rightPanel.add(time);
-        rightPanel.add(Box.createRigidArea(new Dimension(0, 30)));
-        rightPanel.add(addEvent);
-        rightPanel.add(deleteEvent);
+        //left1 layout
+        left1.setLayout(new FlowLayout());
+        left1.add(freeHoursLabel);
+        left1.add(freeHours);
+        left1.add(mon);
+        left1.add(tues);
+        left1.add(wed);
+        left1.add(thur);
+        left1.add(fri);
+        left1.add(sat);
+        left1.add(sun);
+        left1.add(addHour);
+        left1.setBorder(BorderFactory.createLineBorder(Color.black));
+
+        //left2 layout
+        //left2.setBorder(BorderFactory.createLineBorder(Color.GREEN));
 
 
-        //frame.add(addEvent, BorderLayout.EAST);
-        //frame.add(deleteEvent, BorderLayout.WEST);
-        //frame.add(eventName, BorderLayout.WEST);
+        //center layout
+        center.setLayout(new FlowLayout());
+        center.add(timelineLabel);
+
+        //right layout
+        right.add(Box.createRigidArea(new Dimension(0, 50)));
+        right.setLayout(new BoxLayout(right, BoxLayout.PAGE_AXIS));
+        right.add(eventNameLabel);
+        right.add(Box.createRigidArea(new Dimension(0, 5)));
+        right.add(eventName);
+        right.add(Box.createRigidArea(new Dimension(0, 5)));
+        right.add(locationLabel);
+        right.add(Box.createRigidArea(new Dimension(0, 5)));
+        right.add(location);
+        right.add(Box.createRigidArea(new Dimension(0, 5)));
+        right.add(timeLabel);
+        right.add(Box.createRigidArea(new Dimension(0, 5)));
+        right.add(time);
+        right.add(Box.createRigidArea(new Dimension(0, 5)));
+        right.add(addEvent);
+        right.add(Box.createRigidArea(new Dimension(0, 5)));
+        right.add(deleteEvent);
+        right.add(Box.createRigidArea(new Dimension(0, 5)));
+
+        //Basic stuff here
         frame.setTitle("Down4That");
-        //frame.add(location);
-        //frame.add(time);
-
+        frame.setPreferredSize(new Dimension(700, 400));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
+        frame.pack();
         frame.setVisible(true);
+        frame.setResizable(true);
     }
-
 }
